ec43feb0b855fb9086b5c13ba9da22ef60f62edd
KaShTaN27 <dkachan.lida@mail.ru>
2022-10-24 16:54:32 +0300

    Add dateTime generator

---
+++ Faker.Core/Faker.Core/Generator/DateTimeGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace Faker.Core.Generator;
+
+public class DateTimeGenerator : IGenerator
+{
+    public object Generate(Type type)
+    {
+        return new DateTime(RandomS.Instance().NextInt64());
+    }
+}
