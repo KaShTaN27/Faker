46f5970f0960b2d52cc9dee150f42e1806a1d6bc
KaShTaN27 <dkachan.lida@mail.ru>
2022-10-24 15:42:00 +0300

    Add string generator

---
+++ Faker.Core/Faker.Core/Generator/StringGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Faker.Core.Generator;
+
+public class StringGenerator : IGenerator
+{
+    private readonly char[] _chars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    
+    public object Generate(Type type)
+    {
+        var sb = new StringBuilder();
+        var length = RandomS.Instance().Next(3, 15);
+        for (var i = 0; i < length; i++)
+            sb.Append(_chars[RandomS.Instance().Next(0, _chars.Length)]);
+        return sb.ToString();
+    }
+}
