743b885638eab233faa79567563d97942ed3b988
KaShTaN27 <dkachan.lida@mail.ru>
2022-10-24 13:19:59 +0300

    Add bool & double generators

---
+++ Faker.Core/Faker.Core/Generator/BoolGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace Faker.Core.Generator;
+
+public class BoolGenerator : IGenerator
+{
+    public object Generate(Type type)
+    {
+        return true;
+    }
+}
---
+++ Faker.Core/Faker.Core/Generator/DoubleGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace Faker.Core.Generator;
+
+public class DoubleGenerator : IGenerator
+{
+    public object Generate(Type type)
+    {
+        return RandomS.Instance().NextDouble() + 1 * 10;
+    }
+}
