05cf92eaea85af087ef7c7461adb76272cbb9699
KaShTaN27 <dkachan.lida@mail.ru>
2022-10-24 16:54:19 +0300

    Add list generator

---
+++ Faker.Core/Faker.Core/Generator/ListGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using Faker.Core.Exception;
+
+namespace Faker.Core.Generator;
+
+public class ListGenerator : IGenerator
+{
+    private readonly IFaker _faker;
+
+    public ListGenerator(IFaker faker)
+    {
+        _faker = faker;
+    }
+
+    public object Generate(Type type)
+    {
+        var length = RandomS.Instance().Next(3, 8);
+        var list = (IList) (Activator.CreateInstance(type)
+                   ?? throw new ObjectInitializationException("Could not generate list"));
+        for (var i = 0; i < length; i++)
+            list.Add(_faker.Create(type.GetGenericArguments()[0]));
+        return list;
+    }
+}
